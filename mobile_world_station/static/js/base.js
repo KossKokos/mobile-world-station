8bcb9fa996d1c1cf40df94cdfebdc7df0304fe5e
Kostiantyn <132070141+KossKokos@users.noreply.github.com>
2025-05-01 14:31:19 +0100

    rgf

--- mobile_world_station/static/js/base.js
+++ mobile_world_station/static/js/base.js
@@ -6,24 +6,7 @@ document
   nav.style.display = nav.style.display === "none" ? "block" : "none";
 });
 
-function handleLogout(event) {
-event.preventDefault();
-if (confirm("Are you sure you want to logout?")) {
-  // Create a hidden form and submit it
-  const form = document.createElement("form");
-  form.method = "POST";
-  form.action = '{% url "logout" %}';
 
-  const csrf = document.createElement("input");
-  csrf.type = "hidden";
-  csrf.name = "csrfmiddlewaretoken";
-  csrf.value = "{{ csrf_token }}";
-
-  form.appendChild(csrf);
-  document.body.appendChild(form);
-  form.submit();
-}
-}
 
 // Hide mobile menu if screen is resized to desktop size
 window.addEventListener("resize", function () {
